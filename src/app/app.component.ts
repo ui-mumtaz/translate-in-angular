bbefa54ffc206cea5ee327a05971899ec969176f
Mumtaz <mumtaz.ahmed@e2enetworks.com>
2021-01-02 13:44:09 +0530

    added password generator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,16 +7,54 @@ import { TranslateService } from './translate.service';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  text: string;
-  translatedtext: string;
+  length = 0;
+  includeLetters = false;
+  includeNumbers = false;
+  includeSymbols = false;
+  password = '';
 
-  constructor(private translateService: TranslateService) { }
+  onChangeLength(value: string) {
+    const parsedValue = parseInt(value);
 
-  submit() {
-    this.translateService.translate(this.text).subscribe(
-      (result)=> {
-        this.translatedtext = result;
-      }
-    )
+    if (!isNaN(parsedValue)) {
+      this.length = parsedValue;
+    }
+  }
+
+  onChangeUseLetters() {
+    this.includeLetters = !this.includeLetters;
+  }
+
+  onChangeUseNumbers() {
+    this.includeNumbers = !this.includeNumbers;
+  }
+
+  onChangeUseSymbols() {
+    this.includeSymbols = !this.includeSymbols;
+  }
+
+  onButtonClick() {
+    const numbers = '1234567890';
+    const letters = 'abcdefghijklmnopqrstuvwyz';
+    const symbols = '!@#$%^&*()';
+
+    let validChars = '';
+    if (this.includeLetters) {
+      validChars += letters;
+    }
+    if (this.includeNumbers) {
+      validChars += numbers;
+    }
+    if (this.includeSymbols) {
+      validChars += symbols;
+    }
+
+    let generatedPassword = '';
+    for (let i = 0; i < this.length; i++) {
+      const index = Math.floor(Math.random() * validChars.length);
+      generatedPassword += validChars[index];
+    }
+    this.password = generatedPassword;
   }
 }
+
