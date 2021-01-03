748a30ab06be289b42d81fe0d5bd6e2f24da6fbc
Mumtaz <mumtaz.ahmed@e2enetworks.com>
2021-01-03 16:28:51 +0530

    update initial typing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { TranslateService } from './translate.service';
+import { lorem } from 'faker';
 
 @Component({
   selector: 'app-root',
@@ -7,54 +7,18 @@ import { TranslateService } from './translate.service';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  length = 0;
-  includeLetters = false;
-  includeNumbers = false;
-  includeSymbols = false;
-  password = '';
+  randomText = lorem.sentence();
+  enteredText = '';
 
-  onChangeLength(value: string) {
-    const parsedValue = parseInt(value);
-
-    if (!isNaN(parsedValue)) {
-      this.length = parsedValue;
-    }
+  onInput(value: string) {
+    this.enteredText = value;
   }
 
-  onChangeUseLetters() {
-    this.includeLetters = !this.includeLetters;
-  }
-
-  onChangeUseNumbers() {
-    this.includeNumbers = !this.includeNumbers;
-  }
-
-  onChangeUseSymbols() {
-    this.includeSymbols = !this.includeSymbols;
-  }
-
-  onButtonClick() {
-    const numbers = '1234567890';
-    const letters = 'abcdefghijklmnopqrstuvwyz';
-    const symbols = '!@#$%^&*()';
-
-    let validChars = '';
-    if (this.includeLetters) {
-      validChars += letters;
-    }
-    if (this.includeNumbers) {
-      validChars += numbers;
-    }
-    if (this.includeSymbols) {
-      validChars += symbols;
+  compare(randomLetter: string, enteredLetter: string) {
+    if (!enteredLetter) {
+      return 'pending';
     }
 
-    let generatedPassword = '';
-    for (let i = 0; i < this.length; i++) {
-      const index = Math.floor(Math.random() * validChars.length);
-      generatedPassword += validChars[index];
-    }
-    this.password = generatedPassword;
+    return randomLetter === enteredLetter ? 'correct' : 'incorrect';
   }
 }
-
